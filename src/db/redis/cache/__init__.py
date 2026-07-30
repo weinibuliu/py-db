53379a82bcc6ba3280f961b42c9802404ec9c866
weinibuliu <weinibuliu@outlook.com>
2026-07-31 02:34:11 +0800

    chore(redis): 导出 cache

--- src/db/redis/cache/__init__.py
+++ src/db/redis/cache/__init__.py
@@ -2,7 +2,10 @@ from .core import *
 from .model import UserCache
 
 __all__ = [
+    "read_user_cache",
     "set_user_cache",
     "revoke_user_cache",
+    "set_user_caches",
+    "revoke_user_caches",
     "UserCache",
 ]
