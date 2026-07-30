d87e6b86ffa6f512dc78abf50a1b8e35a8cedf85
weinibuliu <weinibuliu@outlook.com>
2026-07-31 06:42:46 +0800

    feat(redis): 提供 get_route_count 方法

--- src/db/redis/analyze/core.py
+++ src/db/redis/analyze/core.py
@@ -1,3 +1,5 @@
+from typing import Optional, overload
+
 from ..client import RedisClient
 from .define import Prefix, TOTAL_COUNT_KEY, TRACEBACK_TTL
 from .model import TracebackDetail
@@ -8,6 +10,14 @@ async def route_count(method: str, route: str):
     async with RedisClient.pipeline() as pipe:
         pipe.incr(TOTAL_COUNT_KEY)
         pipe.incr(Prefix.route(method, route))
+
+
+async def get_total_route_count() -> Optional[str]:
+    return await RedisClient.get(TOTAL_COUNT_KEY)
+
+
+async def get_route_count(method: str, route: str) -> Optional[str]:
+    return await RedisClient.get(Prefix.route(method, route))
 
 
 async def add_traceback(_uuid: str, detail: TracebackDetail):
