b5b9504725e82eeba857aaca07080cb53e93af36
weinibuliu <weinibuliu@outlook.com>
2026-07-31 06:31:34 +0800

    feat(redis): 新增 get_traceback 方法

--- src/db/redis/analyze/core.py
+++ src/db/redis/analyze/core.py
@@ -1,6 +1,7 @@
 from ..client import RedisClient
 from .define import Prefix, TOTAL_COUNT_KEY, TRACEBACK_TTL
 from .model import TracebackDetail
+from ...common import NotFoundError
 
 
 async def route_count(method: str, route: str):
@@ -15,3 +16,13 @@ async def add_traceback(_uuid: str, detail: TracebackDetail):
         detail.model_dump_json(),
         TRACEBACK_TTL,
     )
+
+
+async def get_traceback(_uuid: str) -> TracebackDetail:
+    tb = await RedisClient.get(
+        Prefix.traceback(_uuid),
+    )
+
+    if tb is None:
+        raise NotFoundError()
+    return TracebackDetail.model_validate_json(tb)
