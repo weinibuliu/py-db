29aea0c5cea71c32665469b9ef322c09828b9506
weinibuliu <weinibuliu@outlook.com>
2026-07-31 07:07:56 +0800

    chore(redis): analyze 前缀

--- src/db/redis/analyze/define.py
+++ src/db/redis/analyze/define.py
@@ -8,7 +8,7 @@ TRACEBACK_TTL = 7 * 24 * 3600  # 7d
 class Prefix:
     @staticmethod
     def route(method: str, route: str) -> str:
-        return f"{method}-{ROUTE_COUNT_PREFIX}{route}"
+        return f"A:{method}-{ROUTE_COUNT_PREFIX}{route}"
 
     @staticmethod
     def traceback(_uuid: str) -> str:
