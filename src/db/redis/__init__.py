fc77f004a8f736f998a6ee583191a7a1d68f4fc0
weinibuliu <weinibuliu@outlook.com>
2026-07-31 19:19:02 +0800

    chore: 导出 RedisClient

--- src/db/redis/__init__.py
+++ src/db/redis/__init__.py
@@ -1,11 +1,13 @@
-from .client import close_redis, create_redis
+from .client import close_redis, create_redis, RedisClient
 from .session.define import ACCESS_TTL, REFRESH_TTL
-from . import analyze, cache, session
+from . import analyze, cache, chat_cache, session
 
 __all__ = [
     "analyze",
     "cache",
+    "chat_cache",
     "session",
+    "RedisClient",
     "create_redis",
     "close_redis",
     "ACCESS_TTL",
